473369b4418f1b9ac0fdbde8f9cd7e0877e36634
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-25 21:59:05 +0200

    Add files via upload

---
+++ stpwtch_main.c
@@ -0,0 +1,46 @@
+
+#define F_CPU 3686000
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+
+volatile unsigned char 	i = 0;
+volatile unsigned char 	run = 1;
+volatile unsigned int 	ovf_counter = 0;
+
+
+ISR(INT0_vect) {
+	i = 0;
+}
+
+ISR(INT1_vect) {
+	run = 1-run;
+}
+
+ISR(TIMER0_OVF_vect) {
+	TCNT0 = 72;
+	
+	if(run)	{
+		if(ovf_counter == 19999) {	//20000 interrupts i sekundet..
+			ovf_counter = 0;
+				i++;
+		}
+		else
+			ovf_counter++;
+	}
+}
+
+int main(void) {
+	DDRC  = 0xFF;
+	TCCR0 = 1;				// Timer/Counter Control Register
+	PORTC = 0xFF;
+	TIMSK =  0x01;			// TOIE0
+	MCUCR = 0b1010;   		// ISC01 og ISC11
+	GICR  = 0b11000000;		// INT1 og INT0
+	sei();
+	while(1) {
+		PORTC =~ i;
+	}
+	
+}
