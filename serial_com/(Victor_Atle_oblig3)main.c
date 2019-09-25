bdaedb07bf3f58b9170cff5e95b76563922b46c8
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-25 21:58:48 +0200

    Add files via upload

---
+++ serial_com/(Victor_Atle_oblig3)main.c
@@ -0,0 +1,241 @@
+/*
+
+~*~*~*~ Oblig. 3 AUT-1001 ~*~*~*~
+
+|| Victor Hansen, AU1 |||| Atle Egeland, AU1  ||
+
+Levert: 04.05.2017
+
+uC: ATmega644A.
+IDP: Atmel Studio 7.
+
+*/
+
+#include <avr/io.h>
+#include <stdio.h>
+#include "IncFile1.h"   		// - aka. serlcd.c
+#include <avr/interrupt.h>		// - Bibliotek for interrupts
+#include <util/delay.h>			// - Bibliotek for _delay_ms
+
+#define idle	   0
+#define disp_kort  1
+#define PC         2
+#define linefeed   0x0A
+
+// Array for mottatt/sendt data
+volatile unsigned char mottatt_data[70];
+volatile unsigned char sendt_data[70];
+volatile unsigned char ADC_array[50];
+
+// Array som holder sortert data
+unsigned char A_vect[20];
+unsigned char B_vect[20];
+unsigned char C_vect[20];
+unsigned char D_vect[20];
+
+// Diverse variabler
+volatile unsigned int  modus = 1;
+volatile unsigned int  cnt = 1;
+volatile unsigned int  byte = 0;
+volatile unsigned char i = 0; 
+volatile unsigned char tx_count = 0;
+volatile unsigned char rx_count = 0;
+unsigned int element;
+unsigned int ADC_data;
+unsigned int milli_volt;
+unsigned int freq;
+
+// Interrupt når sending av data er ferdig
+ISR(USART0_TX_vect) {   
+    if(sendt_data[cnt]) {
+            UDR0 = sendt_data[cnt++];
+    }
+}
+
+// Interrupt når mottak av data er ferdig
+ISR(USART0_RX_vect) {
+    if(mottatt_data[byte-2] == 'R' && mottatt_data[byte-1] == linefeed) {
+        byte  = 0;
+        modus = disp_kort;
+    }
+    else mottatt_data[byte++] = UDR0;
+}
+
+// Interrupt ADC 
+ISR(ADC_vect) {
+    ADC_data = ADCW;					    // - hold ADCW value
+    milli_volt = (((unsigned long)ADC_data * 5000)/1023);   // - Regner ut 1000 x spenningen
+    ADCSRA = ADCSRA|0x40;				    // - Starter ny konvertering
+}
+
+// Interrupt Timer2 
+ISR(TIMER2_OVF_vect) {
+    OCR2A = OCR2A;
+}
+ 
+int main(void) {
+DDRB   = 0b11110000;
+DDRC   = 0b11110000;
+DDRD   = 0b00000010;	// - Output PD1(TXD)
+PORTB  = 0b00001111;
+PORTC  = 0b00001111;
+UCSR0A = 0b00000000;	// - Init. USART
+UCSR0B = 0b11011000;	// - RXCIE/TXCIE/RXEN/TXEN
+UBRR0  = 0b01011111;	// - Baudrate = 9600bps
+ADMUX  = 0b00000000;	// - ADC Multiplexer Selection Register
+TCCR2A = 0b01000011;	// - COM2A0/WGM21/WGM20
+TCCR2B = 0b00001100;	// - WGM22/CS22
+OCR2A  = 230;		// - Output Compare Register A
+ADCSRA = 0b11001111;	// - Init. ADC
+
+sei();			// - Init. global interrupt
+init_lcd ();		// - Init. display
+modus = idle;
+_delay_ms(50);
+  
+while (1) {
+	
+    /**** do nothing ****/
+    while(modus == idle) {
+    }
+    /**** Sender data til displaykort ****/
+    while(modus == disp_kort) {
+		 
+    // A) - Behandler data for tekstlinje 1
+        if(mottatt_data[rx_count] = 'A') {
+            i = 0;
+            rx_count++;
+            while(mottatt_data[rx_count] != linefeed) {
+                A_vect[i++] = mottatt_data[rx_count++];
+            }
+            rx_count++;
+        }
+    // A) - Printer tekst (linje 1)
+        lcd_printline(0,0,A_vect);
+        
+    // B) - Behandler data for tekstlinje 2
+        if(mottatt_data[rx_count] = 'B') {
+            i = 0;
+            rx_count++;
+            while(mottatt_data[rx_count] != linefeed) {
+                B_vect[i++] = mottatt_data[rx_count++];
+            }
+            rx_count++;
+        }
+		
+    // B) - Printer klokkeslett (linje 2)
+        lcd_printline(1,0,B_vect);
+        _delay_ms(100);
+		 
+    // C) - Behandler data for LEDs
+        if(mottatt_data[rx_count] = 'C') {
+            i = 0;
+            rx_count++;
+            while(mottatt_data[rx_count] != linefeed) {
+                C_vect[i++] = mottatt_data[rx_count++];
+            }
+            rx_count++;
+        }
+		 
+    // LED 1 (AV/PÅ)
+    if(C_vect[0] == '0') PORTB&=0b01111111;
+    else 		 PORTB|=0b10000000;
+
+    // LED 2 (AV/PÅ)
+    if(C_vect[1] == '0') PORTB&=0b10111111;
+    else 		 PORTB|=0b01000000;
+		
+    // LED 3 (AV/PÅ)
+    if(C_vect[2] == '0') PORTB&=0b11011111;
+    else	         PORTB|=0b00100000;
+		
+    // LED 4 (AV/PÅ)
+    if(C_vect[3] == '0') PORTB&=0b11101111;
+    else 		 PORTB|=0b00010000;
+
+    // D) - Behandler data for piezo-element
+    if(mottatt_data[rx_count] = 'D') {
+        i = 0;
+        rx_count++;
+        while(mottatt_data[rx_count] != linefeed) {
+            D_vect[i++] = mottatt_data[rx_count++];
+        }
+        rx_count++;
+    }
+    rx_count++;
+    
+    // D) - Frekvens for piezo-element
+    if(D_vect[0] == '1') {
+        DDRD |= 0b10000000;
+        freq = ((D_vect[1] - '0')*1000) + ((D_vect[2] - '0')*100)
+            +((D_vect[3] - '0')*10)   + (D_vect[4]  - '0');
+        OCR2A = freq;
+    }
+    else if(D_vect[0] == '0') {
+        DDRD &=~0b10000000;
+    }
+    
+    // Bytter nå modus..
+    modus = PC;
+}
+	
+/**** Sender data til program på PC ****/
+while(modus == PC) {
+    while(sendt_data[tx_count]) {
+        sendt_data[tx_count++] = 0;
+    }
+    tx_count = 0;
+		
+    // A) - ADC
+    itoa(milli_volt, ADC_array, 10);	// - Printer ut spenning i mV
+    sendt_data[0]= 'A';
+    for(element = 0; element < 4; element++) {
+        sendt_data[++tx_count] = ADC_array[element];
+    }
+    sendt_data[++tx_count] = linefeed;
+		
+    // B) - Fritekst
+    volatile unsigned char fri_tekst[]= "1 Protokoll";		// - Her endres friteksten
+    sendt_data[++tx_count] = 'B';
+    element = 0;
+    tx_count++;
+    while(fri_tekst[element] && element < 17) {
+        sendt_data[tx_count++] = fri_tekst[element++];
+					}
+		sendt_data[tx_count++] = linefeed;
+		
+		
+		// C) - DIP-brytere
+		sendt_data[tx_count++] = 'C';
+		
+			// Bryter 1
+			if((PINB & (1<<0)))		sendt_data[tx_count++] = '0';	
+			else					sendt_data[tx_count++] = '1';
+		
+			// Bryter 2
+			if((PINB & (1<<1)))		sendt_data[tx_count++] = '0';	
+			else					sendt_data[tx_count++] = '1';
+		
+			// Bryter 3
+			if((PINB & (1<<2)))		sendt_data[tx_count++] = '0';	
+			else					sendt_data[tx_count++] = '1';
+		
+			// Bryter 4
+			if((PINB & (1<<3)))		sendt_data[tx_count++] = '0';	
+			else					sendt_data[tx_count++] = '1';
+		
+		sendt_data[tx_count++] = linefeed;
+		
+		// D) - Adressebryter
+		sendt_data[tx_count++] = 'D';
+			unsigned char bryter = (PINC&0b00001111);
+			sprintf(&sendt_data[tx_count++],"%x",bryter); 	// -  ASCII til HEX "%x"
+		
+			sendt_data[tx_count++] = linefeed;
+			_delay_ms(40);
+			UDR0  = sendt_data[0];
+			cnt   = 1;
+		modus = idle;
+		}	
+	}
+}
---
+++ serial_com/IncFile1.h
@@ -0,0 +1,268 @@
+/* 
+~~~~~ Ingen endringer her ~~~~~
+
+Victor Hansen, Atle Egeland
+04.05.2017
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+//FOR DOG 3 x 16
+//Definerer hvilke registre og pinner som hører til LCD-pinnene CSB, CLK, SI og RS:
+
+#define lcd_csb_port PORTD
+#define lcd_csb_ddr  DDRD
+#define lcd_csb_pin 4
+
+#define lcd_clk_port PORTD
+#define lcd_clk_ddr DDRD
+#define lcd_clk_pin 5
+
+#define lcd_si_port PORTD
+#define lcd_si_ddr	DDRD
+#define lcd_si_pin  6
+
+#define lcd_rs_port PORTC
+#define lcd_rs_ddr DDRC
+#define lcd_rs_pin 4
+
+//Inkluderer delay-funksjoner, kan utelates dersom inkludert andre steder:
+
+#define F_CPU 14745600
+#include <util/delay.h>
+
+//Enable-signal ------------------------------------------------------------------------------
+void strobe(void){
+	//_delay_us(1);          //må kanskje inkluderes for raske klokker! Progget for 14.745Mhz
+	lcd_clk_port&=~(1<<lcd_clk_pin);
+	//_delay_us(1);
+	lcd_clk_port|=(1<<lcd_clk_pin);
+}
+
+//Sette instruksjoner ut i en SPI-struktur ---------------------------------------------------
+void lcdinst(unsigned char txb, unsigned char RS){
+	lcd_csb_port&=~(1<<lcd_csb_pin);
+	lcd_rs_port&=~(1<<lcd_rs_pin);
+	lcd_rs_port|=(RS<<lcd_rs_pin);
+	
+	if (txb&128)lcd_si_port|=(1<<lcd_si_pin); else lcd_si_port&=~(1<<lcd_si_pin);  strobe();
+	if (txb&64) lcd_si_port|=(1<<lcd_si_pin); else lcd_si_port&=~(1<<lcd_si_pin);  strobe();
+	if (txb&32) lcd_si_port|=(1<<lcd_si_pin); else lcd_si_port&=~(1<<lcd_si_pin);  strobe();
+	if (txb&16) lcd_si_port|=(1<<lcd_si_pin); else lcd_si_port&=~(1<<lcd_si_pin);  strobe();
+	if (txb&8)	lcd_si_port|=(1<<lcd_si_pin); else lcd_si_port&=~(1<<lcd_si_pin);  strobe();
+	if (txb&4)	lcd_si_port|=(1<<lcd_si_pin); else lcd_si_port&=~(1<<lcd_si_pin);  strobe();
+	if (txb&2)	lcd_si_port|=(1<<lcd_si_pin); else lcd_si_port&=~(1<<lcd_si_pin);  strobe();
+	if (txb&1)	lcd_si_port|=(1<<lcd_si_pin); else lcd_si_port&=~(1<<lcd_si_pin);  strobe();
+	
+	lcd_csb_port|=(1<<lcd_csb_pin);
+}
+
+//funksjon som brukes for å sette cg-ram-adresse mtp å definere egne tegn
+void cgramaddressset(unsigned char cgaddress,unsigned char line){
+	lcdinst(cgaddress*8+line+64,0);    _delay_us(40);
+}
+
+//funksjon for å sette cursoren på i displayet
+void lcd_cursoron(void){
+	lcdinst(0b00001110,0);    _delay_us(40);
+}
+
+//og cursor av....
+void lcd_cursoroff(void){
+	lcdinst(0b00001100,0);    _delay_us(40);
+}
+
+//en vanvittig lang funksjon for å definere cg-ram'en. Kan gjøres mye enklere.
+void init_cgram(void){
+	int cgaddress,line;
+	for (cgaddress=0;cgaddress<8;cgaddress=cgaddress+1) {
+		for (line=0;line<8;line=line+1) {
+			cgramaddressset(cgaddress,line);
+			switch(cgaddress){
+				case 0:
+				switch(line){       //æ
+					case 0: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 1: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 2: lcdinst(0b00001110,1);    _delay_us(40);      break;
+					case 3: lcdinst(0b00010101,1);    _delay_us(40);      break;
+					case 4: lcdinst(0b00001111,1);    _delay_us(40);      break;
+					case 5: lcdinst(0b00010100,1);    _delay_us(40);      break;
+					case 6: lcdinst(0b00001111,1);    _delay_us(40);      break;
+					case 7: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					default: break;
+				};
+				case 1:
+				switch(line){       //ø
+					case 0: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 1: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 2: lcdinst(0b00001110,1);    _delay_us(40);      break;
+					case 3: lcdinst(0b00010011,1);    _delay_us(40);      break;
+					case 4: lcdinst(0b00010101,1);    _delay_us(40);      break;
+					case 5: lcdinst(0b00011001,1);    _delay_us(40);      break;
+					case 6: lcdinst(0b00001110,1);    _delay_us(40);      break;
+					case 7: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					default: break;
+				};
+				case 2:
+				switch(line){       //å
+					case 0: lcdinst(0b00000100,1);    _delay_us(40);      break;
+					case 1: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 2: lcdinst(0b00001110,1);    _delay_us(40);      break;
+					case 3: lcdinst(0b00000001,1);    _delay_us(40);      break;
+					case 4: lcdinst(0b00001111,1);    _delay_us(40);      break;
+					case 5: lcdinst(0b00010001,1);    _delay_us(40);      break;
+					case 6: lcdinst(0b00001111,1);    _delay_us(40);      break;
+					case 7: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					default:  break;
+				};
+				case 3:
+				switch(line){       //Æ
+					case 0: lcdinst(0b00000111,1);    _delay_us(40);      break;
+					case 1: lcdinst(0b00001100,1);    _delay_us(40);      break;
+					case 2: lcdinst(0b00010100,1);    _delay_us(40);      break;
+					case 3: lcdinst(0b00011111,1);    _delay_us(40);      break;
+					case 4: lcdinst(0b00010100,1);    _delay_us(40);      break;
+					case 5: lcdinst(0b00010100,1);    _delay_us(40);      break;
+					case 6: lcdinst(0b00010111,1);    _delay_us(40);      break;
+					case 7: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					default:  break;
+				};
+				
+				case 4:
+				switch(line){       //Å
+					case 0: lcdinst(0b00000100,1);    _delay_us(40);      break;
+					case 1: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 2: lcdinst(0b00000100,1);    _delay_us(40);      break;
+					case 3: lcdinst(0b00001010,1);    _delay_us(40);      break;
+					case 4: lcdinst(0b00001110,1);    _delay_us(40);      break;
+					case 5: lcdinst(0b00010001,1);    _delay_us(40);      break;
+					case 6: lcdinst(0b00010001,1);    _delay_us(40);      break;
+					case 7: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					default: break;
+				};
+				case 5:
+				switch(line){       // nej - surtrut
+					case 0: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 1: lcdinst(0b00001010,1);    _delay_us(40);      break;
+					case 2: lcdinst(0b00001010,1);    _delay_us(40);      break;
+					case 3: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 4: lcdinst(0b00010001,1);    _delay_us(40);      break;
+					case 5: lcdinst(0b00010001,1);    _delay_us(40);      break;
+					case 6: lcdinst(0b00001110,1);    _delay_us(40);      break;
+					case 7: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					default: break;
+				};
+				case 6:
+				switch(line){       // jej - smiley
+					case 0: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 1: lcdinst(0b00001010,1);    _delay_us(40);      break;
+					case 2: lcdinst(0b00001010,1);    _delay_us(40);      break;
+					case 3: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					case 4: lcdinst(0b00001110,1);    _delay_us(40);      break;
+					case 5: lcdinst(0b00010001,1);    _delay_us(40);      break;
+					case 6: lcdinst(0b00010001,1);    _delay_us(40);      break;
+					case 7: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					default: break;
+				};
+				case 7:
+				switch(line){       // lighting
+					case 0: lcdinst(0b00000001,1);    _delay_us(40);      break;
+					case 1: lcdinst(0b00000010,1);    _delay_us(40);      break;
+					case 2: lcdinst(0b00000100,1);    _delay_us(40);      break;
+					case 3: lcdinst(0b00000010,1);    _delay_us(40);      break;
+					case 4: lcdinst(0b00010100,1);    _delay_us(40);      break;
+					case 5: lcdinst(0b00011000,1);    _delay_us(40);      break;
+					case 6: lcdinst(0b00011100,1);    _delay_us(40);      break;
+					case 7: lcdinst(0b00000000,1);    _delay_us(40);      break;
+					default: break;
+				};
+				default: break;
+			};
+		};
+	};
+}
+
+//init lcd, setter rette pinner ut osv...
+void init_lcd(void){
+	//_delay_ms(20);
+	lcd_rs_ddr	|=(1<<lcd_rs_pin);
+	lcd_clk_ddr	|=(1<<lcd_clk_pin);
+	lcd_si_ddr	|=(1<<lcd_si_pin);
+	lcd_csb_ddr	|=(1<<lcd_csb_pin);
+	
+	lcd_rs_port	|=(1<<lcd_rs_pin);
+	lcd_clk_port|=(1<<lcd_clk_pin);
+	lcd_si_port	|=(1<<lcd_si_pin);
+	lcd_csb_port|=(1<<lcd_csb_pin);
+	_delay_ms(40);
+
+	lcdinst(0b00111001,0); _delay_us(1100);
+	lcdinst(0b00011101,0); _delay_us(30);
+	lcdinst(0b01010010,0); _delay_us(30);
+	lcdinst(0b01101001,0); _delay_us(30);
+	lcdinst(0b01110100,0); _delay_us(30);
+	lcdinst(0b00111000,0); _delay_us(30);
+	lcdinst(0b00001100,0); _delay_us(30);
+	lcdinst(0b00000001,0); _delay_us(1100);
+	lcdinst(0b00000110,0); _delay_us(30);
+
+	init_cgram();
+}
+
+//setter cursoren på rett sted, row kan være 0, 1 eller 2, col 0->15
+void dotpos(int row, int col){  //sets marker ready to write on row,col
+	unsigned char instr=0;
+	switch(row){
+		case 0: instr=0x80; break;
+		case 1: instr=0x90; break;
+		case 2: instr=0xA0; break;
+	default: break;}
+	lcdinst(instr+col%16,0);  _delay_us(39);
+}
+
+//funksjon som skriver ut en tekst på plassen [row,col]
+void lcd_printline (char row, char col, char *a){
+	dotpos(row,col);
+	while(*a){
+		if     (*a=='Æ')		lcdinst(0b10010010,1);
+		else if(*a=='æ') lcdinst(0b10010001,1);
+		else if(*a=='Ø') lcdinst(0b11101110,1);
+		else if(*a=='ø') lcdinst(0b11101111,1);
+		else if(*a=='Å') lcdinst(0b10001111,1);
+		else if(*a=='å') lcdinst(0b10000110,1);
+		else
+		lcdinst(*a,1);
+		_delay_us(43);
+		*a++;
+	}
+}
+
+//funksjon for å skrive ut en og en bokstav
+void lcd_printchar (unsigned char a){
+	if(a=='Æ')		lcdinst(0b10010010,1);
+	else if(a=='æ') lcdinst(0b10010001,1);
+	else if(a=='Ø') lcdinst(0b11101110,1);
+	else if(a=='ø') lcdinst(0b11101111,1);
+	else if(a=='Å') lcdinst(0b10001111,1);
+	else if(a=='å') lcdinst(0b10000110,1);
+	else lcdinst(a,1);
+	_delay_us(43);
+}
+
+//funksjon for å skrive ut tall på plass [row, col]. Tallet har d siffer, og tallet er nb
+void lcd_printnb(char row, char col, char d,long int nb){
+	char tobeprinted;
+	char i;
+	long int fact;
+	dotpos(row,col);
+	while(d>0){
+		fact=1;
+		for(i=1;i<d;i++)fact=fact*10;
+		tobeprinted=nb/fact;
+		lcdinst(tobeprinted+48,1); _delay_us(43);
+		nb=nb-(tobeprinted*fact);
+		d--;
+	}
+	
+	
+	
+}
