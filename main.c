10da7e3fbd78a1787b4a250a093a8c8227fc3fee
Victor Hansen <0vjhansen@gmail.com>
2020-11-12 01:20:52 +0100

    ignore

--- main.c
+++ main.c
@@ -5,31 +5,25 @@
 #define F_CPU 1000000UL
 #include <util/delay.h>
 
-	
 int main(void)
-{	
-    DDRB  |= (1<<PB1 | 1<<PB0);		// PORTD = output
+{ 
+    DDRB  |= (1<<PB1 | 1<<PB0);   // PORTD = output
     PORTB = 0x00;
-    
-    TCCR1A = 0x00;		// Normal
-    TCCR1B = (1<<CS12 | 1<<CS10);		// 1024 prescaler
-
-         // For 1 sec we need to count to ( (1 sec * 1000000 MHz) / 1024) = 0x03D0
+    TCCR1A = 0x00;    // Normal
+    TCCR1B = (1<<CS12 | 1<<CS10);   // 1024 prescaler
+    // For 1 sec we need to count to ( (1 sec * 1000000 MHz) / 1024) = 0x03D0
     OCR1AH = 0x03; // Output Compare Registers (16 bit)
     OCR1AL = 0xD0;
-    
     TCNT1H = 0x00;  // Timer/Counter count/value registers (16 bit)
     TCNT1L = 0x00;
     TIMSK1 = (1<<OCIE1A);  
    
-    sei();			// Enable global interrupt
+    sei();      // Enable global interrupt
     
-    while (1) {		}
+    while (1) {}
 }
 
-
-
 ISR(TIMER1_COMPA_vect) 
-{ 	
+{   
     PORTB ^= (1<<PB1 | 1<<PB0);
 }
