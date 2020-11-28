7bbd3eef31afcbe7b00b9e7a87b0eef374f8d33d
Victor Hansen <0vjhansen@gmail.com>
2020-11-28 23:10:37 +0100

    upload

--- ADC/adc_main.c
+++ ADC/adc_main.c
@@ -13,15 +13,6 @@ volatile unsigned char hyperText[32];
 #define LF  0x0A // Line feed
 #define SPACE 0x20
 
-
-#define THERMISTORNOMINAL 10000      
-// temp. for nominal resistance (almost always 25 C)
-#define TEMPERATURENOMINAL 25   
-// The beta coefficient of the thermistor (usually 3000-4000)
-#define BCOEFFICIENT 3950
-// the value of the 'other' resistor
-#define SERIESRESISTOR 10000    
-
 unsigned int Ctemp;
 
 void USART0_SETUP_9600_BAUD();
